b4058c173b08e4ec9dd3fdfbe627fa07e281e669
andrew-benet <andrewbenet1234@gmail.com>
2024-11-19 19:32:24 +0000

    game.h fixes

--- Game.h
+++ Game.h
@@ -2,23 +2,27 @@
 #define GAME_H
 #include "Team.h"
 
+#include <cstddef>
+#include <string>
+#include <vector>
+
 class Game
 {
-public:
-    Game(int game_number, Team &team1, Team &team2);
-
-    void play_game();
-    bool is_eliminated() const;
-    int get_game_number() const;
-
 private:
     int _game_number;
     bool _is_eliminated;
     Team &_team1,
-        Team &_team2;
+    Team &_team2;
 
-    int determine_round_winner(Team &team1, Team &team2) const {
-    };
+public:
+    Game(int game_number, Team &team1, Team &team2);
+
+    
+    void play_game();
+    bool is_eliminated() const;
+    int get_game_number() const;
+    int determine_round_winner(Team &team1, Team &team2) const { };
+
 };
 
 #endif // GAME_H
