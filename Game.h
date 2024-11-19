06230e7905583693d1c2b7fc7caf2bd04ec786db
andrew-benet <andrewbenet1234@gmail.com>
2024-11-19 11:22:00 -0800

    Implement game.h

--- Game.h
+++ Game.h
@@ -1,7 +1,24 @@
-#ifndef GAME
-#define GAME
+#ifndef GAME_H
+#define GAME_H
+#include "Team.h"
 
-class Game;
+class Game
+{
+public:
+    Game(int game_number, Team &team1, Team &team2);
 
+    void play_game();
+    bool is_eliminated() const;
+    int get_game_number() const;
 
-#endif
+private:
+    int _game_number;
+    bool _is_eliminated;
+    Team &_team1,
+        Team &_team2;
+
+    int determine_round_winner(Team &team1, Team &team2) const {
+    };
+};
+
+#endif // GAME_H
