4a27ed6418e27aabea621b2d885c6ac8553cc30c
Parkman145 <ParkerLearmonth@gmail.com>
2024-11-21 11:08:47 -0800

    Add Tournament declarations

--- Tournament.h
+++ Tournament.h
@@ -1,6 +1,8 @@
 #include <vector>
+#include <string>
 
 #include "Team.h"
+#include "Game.h"
 
 using namespace std;
 
@@ -9,8 +11,17 @@ using namespace std;
 class Tournament{
     public:
         Tournament(vector<Team> teams);
+        void start_tournament();
+        vector<int> get_scores();
+        string format_scores();
+        vector<Team> get_teams();
+        string format_teams();
+        string format_rules();
 
     private:
+        vector<Team> _teams;
+        vector<Game> _games;
+
 
 };
 
